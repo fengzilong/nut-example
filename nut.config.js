03ea30774a46f1d13e619ec57e881dfe7f60c418
fengzilong <feng_zilong@163.com>
2019-04-18 16:28:59 +0800

    init

---
+++ nut.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  zh: 'NUT 项目',
+  en: 'NUT PROJECT',
+  layout: 'now',
+  sidebar: [
+    {
+      icon: '',
+      title: '菜单',
+      children: [
+        'pages/foo',
+        'pages/bar',
+      ]
+    },
+
+    {
+      icon: '',
+      title: 'GitHub',
+      link: 'https://github.com/fengzilong/nut'
+    },
+  ],
+}
